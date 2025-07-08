cba7fa9d08334de8b8bfbc8ce9d98d7d00ca58ac
sahebbali <saheb1996jmr@gmail.com>
2025-07-08 18:27:42 +0600

    add firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth/screens/login_screen.dart';
 import 'package:flutter_firebase_auth/screens/signup_email_password_screen.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
